fbd0226580c956e0b130b6dbbeb4000d08b1e50b
DerMoehre <moehre2507@gmail.com>
2022-10-21 17:56:38 +0200

    removed else statement

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -37,7 +37,7 @@ while (user_move < 0) or (user_move > 2):
         user_move = int(input("What do you choose for rock 0, for paper 1 for scissors 2.:-"))
     except ValueError:
         print("Please insert a valid command")
-        
+
 print(game_ascii[user_move])
 
 computer_move = random.randint(0,2)
@@ -55,7 +55,6 @@ elif user_move > computer_move:
     print("You Win")
 elif computer_move == user_move:
     print("it's a draw")
-else:
-    print("You Typed an invalid number")
+
 
 
