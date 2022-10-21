024bc5d52ed310e3d9340d915994fc4c7dfb38da
DerMoehre <moehre2507@gmail.com>
2022-10-21 18:11:26 +0200

    game ends, if user enters wrong number

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -28,33 +28,38 @@ Scissors=('''
 ''')
 
 # set initial value for user_move
-user_move = -1
 
 game_ascii = [Rock, Paper, Scissors]
 
-while (user_move < 0) or (user_move > 2):
-    try:
-        user_move = int(input("What do you choose for rock 0, for paper 1 for scissors 2.:-"))
-    except ValueError:
-        print("Please insert a valid command")
+try:
+    user_move = int(input("What do you choose for rock 0, for paper 1 for scissors 2.:-"))
+    if (user_move < 0) or (user_move > 2):
+        print("Please insert a valid number")
+    print(game_ascii[user_move])
+    computer_move = random.randint(0,2)
 
-print(game_ascii[user_move])
+    print("computer chose")
+    print(game_ascii[computer_move])
 
-computer_move = random.randint(0,2)
+    if user_move == 0 and computer_move == 2:
+        print("You Win!")
+    elif computer_move == 0 and user_move == 2:
+        print("You lose")
+    elif computer_move > user_move:
+        print("You Lose")
+    elif user_move > computer_move:
+        print("You Win")
+    elif computer_move == user_move:
+        print("it's a draw")
 
-print("computer chose")
-print(game_ascii[computer_move])
+except ValueError:
+    print("Please insert a valid number")
 
-if user_move == 0 and computer_move == 2:
-    print("You Win!")
-elif computer_move == 0 and user_move == 2:
-    print("You lose")
-elif computer_move > user_move:
-    print("You Lose")
-elif user_move > computer_move:
-    print("You Win")
-elif computer_move == user_move:
-    print("it's a draw")
+except IndexError:
+    pass
+
+
+
 
 
 
