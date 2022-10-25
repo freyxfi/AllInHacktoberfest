0837e41f7c1241c6bd7a580d6193f437dbf37aef
SURANJAN DAS <91688087+developerSuranjan@users.noreply.github.com>
2022-10-25 12:57:10 +0530

    New game balloon pop is added

---
+++ ballon_pop.py
@@ -0,0 +1,52 @@
+from turtle import *
+
+#Diameter-Present diameter
+#Pop_diameter: Diameter when the ballon is popped
+
+diameter = 20
+pop_diameter = 100
+color_list = ["red", "black", "blue", "yellow", "green"]
+cur_color_index = 0
+
+
+#Function to create the balloon
+def draw_ballon_func():
+    color(color_list[cur_color_index])
+    dot(diameter)
+
+
+#Function to change the color of the balloon
+def change_color():
+    global cur_color_index
+    global diameter
+    cur_color_index += 1
+    cur_color_index = cur_color_index % 5
+    color(color_list[cur_color_index])
+    dot(diameter)
+
+
+#Function to inflate the balloon
+def inflate_balloon():
+    write("")
+    global diameter
+    diameter += 10
+    draw_ballon_func()
+
+    if diameter > pop_diameter:
+        clear()
+        diameter = 20
+        write("POP!")
+
+
+def deflate_balloon():
+    global diameter
+    diameter -= 10
+    draw_ballon_func()
+
+
+draw_ballon_func()
+onkey(inflate_balloon, 'Up')
+onkey(deflate_balloon, 'Down')
+onkey(change_color, 'Right')
+listen()
+mainloop()
